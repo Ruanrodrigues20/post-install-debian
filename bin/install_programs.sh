b51daf7f3242317aa518aa44873b37342ac508d9
Ruan Rodrigues <ruan@localhost.localdomain>
2025-03-05 08:17:54 -0300

    up

--- bin/install_programs.sh
+++ bin/install_programs.sh
@@ -6,22 +6,51 @@ VERSION="1.0.0"
 
 # List of packages to be installed
 PACKAGES=(
+    "wget"
     "curl"
     "git"
     "vim"
     "build-essential"
+    "bash-complemetion"
     "vlc"
     "python3-pip"
     "gnome-tweaks"
     "gnome-shell-extension-manager"
     "maven"
+    "python3-poetry"
     "flatpak"
     "tree"
     "dpkg"
-    "wget"
     "gnome-software-plugin-flatpak"
     "snap"
     "snapd"
+)
+
+PACKAGES_GARBAGE=(
+	"firefox-esr"
+    "gnome-2048"
+    "gnome-chess"
+    "gnome-sushi"
+    "gnome-sudoku"
+    "gnome-tetravex"
+    "gnome-mines"
+    "five-or-more"
+    "kasumi"
+    "four-in-a-row"
+    "goldendict"
+    "hitori"
+    "libreoffice-core" 
+    "libreoffice-common"
+    "lightsoff"
+    "gnome-mines"
+    "xterm"
+    "mlterm"
+    "gnome-nibbles"
+    "gnome-robots"
+    "swell-foop"
+	"tali"
+	"gnome-taquin"
+	"gnome-tetravex"
 )
 
 # Function to check if the script is running as root
@@ -38,19 +67,38 @@ install_packages() {
     sudo apt update
     
     for package in "${PACKAGES[@]}"; do
-        echo "Installing $package..."
         sudo apt install -y "$package"
     done
 
     echo "Installation completed."
 }
 
+install_firefox(){
+	sudo install -d -m 0755 /etc/apt/keyrings
+	wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+	gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nO fingerprint da chave corresponde ("$0").\n"; else print "\nFalha na verificação: o fingerprint ("$0") não corresponde ao esperado.\n"}'
+	echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+	echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla 
+	sudo apt-get update && sudo apt-get install firefox
+}
+
+remove_packages(){
+    for package in "${PACKAGES_GARBAGE[@]}"; do
+        sudo apt install -y "$package"
+    done
+}
+
 main(){
     # Check if the script is running as root
     check_root
-
     # Install packages
     install_packages
+    install_firefox
+    remove_packages
 }
 
 main
